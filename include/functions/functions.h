3798df6b60ecc3f83ff89748b181ea7bbad58667
AntipinVladislav <125849668+AntipinVladislav@users.noreply.github.com>
2023-02-20 11:52:55 +0400

    Начало своей лабы

--- include/functions/functions.h
+++ include/functions/functions.h
@@ -1,3 +1,30 @@
 #pragma once
 
-int sum_stub(int lhs, int rhs);
+namespace function {
+	enum FigureType {
+		Circle,
+		Triangle,
+		Rectangle
+	};
+
+	class Figure {
+	private:
+		FigureType current_type;
+		float coordinates[5];
+		
+		
+	public:
+		void set_coordinates(Figure current_figure, float* coordinates);
+
+		float get_perimeter(Figure current_figure);
+
+		float get_area(Figure current_figure);
+
+		float find_minimal_outer_rectangle(Figure current_figure);
+
+		float get_type(Figure current_figure);
+
+		void get_coordinates(Figure current_figure, float* coordinates);
+	};
+	
+}
--- src/functions.cc
+++ src/functions.cc
@@ -1,5 +1,30 @@
 #include <functions/functions.h>
 
-int sum_stub(int lhs, int rhs) {
-    return lhs + rhs;
+#include <cmath>
+
+using namespace function;
+
+void Figure::set_coordinates(Figure current_figure, float* points) {
+	for(int i = 0; i < 6; i++)
+	{
+		current_figure.coordinates[i] = points[i];
+	}
+}
+
+void Figure::get_coordinates(Figure current_figure, float* points) {
+	for (int i = 0; i < 6; i++)
+	{
+		points[i] = current_figure.coordinates[i];
+	}
+}
+
+
+
+float Figure::get_perimeter(Figure current_figure) {
+	switch (current_figure.current_type)
+	{
+	case Circle:
+		return 2 * 3.14 * current_figure.coordinates[0];
+	}
+
 }
